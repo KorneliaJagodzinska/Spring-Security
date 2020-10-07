49fa84f6ef64f1eb85f83619b083fd91f677363b
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-07 12:55:31 +0200

    sign-up.html

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,23 +1,35 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class UserController {
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello";
     }
 
-    @GetMapping("/hello-admin")
+    @GetMapping("/for-admin")
     public String forAdmin(){
         return "hello admin";
     }
 
-    @GetMapping("/hello-user")
+    @GetMapping("/for-user")
     public String forUser(){
         return "hello user";
+    }
+    @GetMapping("/sign-up")
+    public String signUp(Model model){
+        model.addAttribute("user", new AppUser());
+        return "sign-up";
+    }
+    @GetMapping("/register")
+    public String register(AppUser appUser){
+        return "sign-up";
     }
 }
